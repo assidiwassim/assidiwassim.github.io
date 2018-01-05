057cbfe25642cb7c7637fddd34bffbf00830ae26
assidiwassim <assidi.wassim.fsm@hotmail.com>
2018-01-05 22:51:23 +0100

    jk

--- js/index.js
+++ js/index.js
@@ -3,22 +3,47 @@
             $('.carousel').carousel();
             
             
-            
+       var a=0;     
+
   $('.button-collapse').sideNav({
       menuWidth: 250, // Default is 300
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true, // Choose whether you can drag to open on touch screens,
-      onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
-      onClose: function(el) { /* Do Stuff  */ }, // A function to be called when sideNav is closed
+      onOpen: function(el) {    
+          a=1; 
+          console.log(a);
+                           }, 
+      onClose: function(el) { 
+          a=0 ;
+          console.log(a);}, 
     }
   );
             
             
+
+var myElement = document.getElementById('home');
+         
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(myElement);
+
+// listen to events...
+mc.on(" panright ", function(ev) {
+   if (a==0){
+     $('.button-collapse').sideNav('show');
+   }
+});  
             
+
             
-    });
+});
         
 function toast(){
     Materialize.toast('cette fonctionnalité n\'est pas disponible maintenant.', 4000);
 }
+
+
+
+
