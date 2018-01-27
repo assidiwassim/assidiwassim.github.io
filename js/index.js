9b9b67bab7aa11f9f7361707fa9037d652060fcd
assidiwassim <assidi.wassim.fsm@hotmail.com>
2018-01-27 16:32:09 +0100

    zejk,c

--- js/index.js
+++ js/index.js
@@ -1,52 +1,10 @@
-        $(document).ready(function(){
-      $('.parallax').parallax();
+        $(document).ready(function() {
+            $('.parallax').parallax();
             $('.carousel').carousel();
-            
-            
-       var a=0;     
-
-  $('.button-collapse').sideNav({
-      menuWidth: 250, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true, // Choose whether you can drag to open on touch screens,
-      onOpen: function(el) {    
-          a=1; 
-            }, 
-      onClose: function(el) { 
-          a=0 ;
-      }, 
-    }
-  );
-            
 
 
-
-// listen to events...
-$(document.body).hammer().on(" panright ", function(ev) {
-     if ($(window).width() < 980) {
-   if (a==0){
-       if(ev.gesture.distance>60)
-     $('.button-collapse').sideNav('show');
-   }}
-     
-});  
-            
-
-// smooth scroll
-   $('.scrollspy').scrollSpy({
-       scrollOffset:50
-   });     
-        });       
-
-
-   
-// toast message
-function toast(){
-    Materialize.toast('cette fonctionnalité n\'est pas disponible maintenant.', 4000);
-}
-
-
-
-
-
+            // smooth scroll
+            $('.scrollspy').scrollSpy({
+                scrollOffset: 50
+            });
+        });
