6557f41c25694cffff63ad3ab663f498fdbc36df
assidiwassim <assidi.wassim.fsm@hotmail.com>
2018-01-27 16:34:16 +0100

    lka,dz

--- js/index.js
+++ js/index.js
@@ -3,8 +3,29 @@
             $('.carousel').carousel();
 
 
+
+
+            $('.button-collapse').sideNav({
+                menuWidth: 250, // Default is 300
+                edge: 'left', // Choose the horizontal origin
+                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true, // Choose whether you can drag to open on touch screens, 
+            });
+
+
+
+
+
+
             // smooth scroll
             $('.scrollspy').scrollSpy({
                 scrollOffset: 50
             });
         });
+
+
+
+        // toast message
+        function toast() {
+            Materialize.toast('cette fonctionnalité n\'est pas disponible maintenant.', 4000);
+        }
