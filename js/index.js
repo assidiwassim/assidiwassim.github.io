e4cecde4fd813de48421aea87d01b36763dec77a
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2018-10-23 11:11:43 +0100

    01

--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
+
+
 $(document).ready(function(){$('.parallax').parallax();$('.carousel').carousel();$('.button-collapse').sideNav({menuWidth:250,edge:'left',closeOnClick:true,draggable:true,});$('.scrollspy').scrollSpy({scrollOffset:50});});function toast(){Materialize.toast('cette fonctionnalité n\'est pas disponible maintenant.',4000);}
